d8011e52d8d405cb4a4e357465e024365f155cdb
Dileep Varma <47983175+dileepvarmaa@users.noreply.github.com>
2019-04-18 22:04:08 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,7 +1,21 @@
-a = input()
-b = input()
-def gcd(a,b):
-	if a==0:
-		return b
-	return gcd(b%a,a)
-print(gcd(a,b))
+a = int(input())
+factors_a=[]
+for i in range(1,a+1):
+	if a%i==0:
+		factors_a.append(i)
+print(factors_a)
+
+
+b = int(input())
+factors_b=[]
+for j in range(1,b+1):
+	if b%j==0:
+		factors_b.append(j)
+print(factors_b)
+
+x = (set(factors_a) & set(factors_b))
+print(x)
+
+largest_common_factor = max(x)
+print(largest_common_factor)
+
