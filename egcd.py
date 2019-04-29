cbe23070e4d1d157763728b505cfce87185baea8
Dileep Varma <47983175+dileepvarmaa@users.noreply.github.com>
2019-04-29 17:22:28 +0530

    Update egcd.py

--- egcd.py
+++ egcd.py
@@ -1,30 +1,25 @@
-a = int(input())
-factors_a=[]
-for i in range(1,a+1):
-	if a%i==0:
-		factors_a.append(i)
-print(factors_a)
-
-
-b = int(input())
-factors_b=[]
-for j in range(1,b+1):
-	if b%j==0:
-		factors_b.append(j)
-print(factors_b)
-g = (set(factors_a) & set(factors_b))
-print(g)
-
-greatest_common_divisor = max(g)
-print(greatest_common_divisor)
-
-def egcd(x,y):
-	for x in range(1,1000):
-		for y in range(1,1000):
-			if (a*x + b*y) == greatest_common_divisor:	
-				return(x,y)
-				break
-
-print(egcd(a,b))
-
+dividend = int(input())
+divisor = int(input())
+q = dividend//divisor
+r = dividend % divisor
+dividend1 = 1
+dividend2 = 0
+divisor1 = 0 
+divisor2 = 1
+remainder1 = dividend1 - (q*divisor1)
+remainder2 = dividend2 - (q*divisor2)
+def egcd(dividend,dividend1,divisor2,divisor,divisor1,remainder1,remainder2,dividend2,r):
+	while(r!=2):	
+		dividend = divisor 
+		divisor = r
+		q = dividend // divisor
+		r = dividend % divisor
+		dividend1 = divisor1
+		divisor1 = remainder1
+		remainder1 = dividend1 - (q*divisor1)
+		dividend2 = divisor2
+		divisor2 = remainder2
+		remainder2 = dividend2 - (q*divisor2)
+	return(remainder1,remainder2)
+print(egcd(dividend,dividend1,divisor2,divisor,divisor1,remainder1,remainder2,dividend2,r))
 
